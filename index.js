d56ded7c2697c67f7363d434437f4c2df937b7e2
Vlad <voiteh44@gmail.com>
2024-01-29 23:27:26 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+let timerId; // переменная, которая будет хранить ID таймера
+
+
+const startButton = document.getElementById("start");
+startButton.addEventListener('click', function () {
+   // НУЖНО ПОГУГЛИТЬ ЧТО ТАКОЕ setInterval
+   if (timerId) clearInterval(timerId);
+   timerId = setInterval(updateClock, 1000); // запускаем  updateClock() каждую секунду
+   console.log(timerId);
+});
+
+
+const stopButton = document.getElementById("stop");
+stopButton.addEventListener('click', function () {
+   // НУЖНО ПОГУГЛИТЬ ЧТО ТАКОЕ clearInterval
+   clearInterval(timerId); // останавливаем таймер
+   console.log(timerId);
+
+});
+
+
+function updateClock() {
+   const clock = document.getElementById('clock');
+   // НУЖНО ПОГУГЛИТЬ ЧТО ТАКОЕ new Date()
+   const now = new Date();
+   const hours = now.getHours()
+   const minutes = now.getMinutes()
+   const seconds = now.getSeconds()
+   clock.textContent = hours + ':' + minutes + ':' + seconds;
+}
